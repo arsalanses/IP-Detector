bea118b7d4cf39b4fde407d6a0ebe67511562404
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-11-05 09:44:56 +0330

    Add count var to simplify

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
-resource "arvan_iaas_abrak" "abrak-1" {
+resource "arvan_iaas_abrak" "abrak" {
   region = var.region
   flavor = "g1-1-1-0"
-  name   = "${var.abrak_name}-1"
+  count  = 3
+  name   = "${var.abrak_name}-${count.index}"
   image {
     type = "distributions"
     name = "debian/12"
@@ -9,56 +10,17 @@ resource "arvan_iaas_abrak" "abrak-1" {
   disk_size = 25
 }
 
-resource "arvan_iaas_abrak" "abrak-2" {
-  region = var.region
-  flavor = "g1-1-1-0"
-  name   = "${var.abrak_name}-2"
-  image {
-    type = "distributions"
-    name = "debian/12"
-  }
-  disk_size = 25
-}
-
-resource "arvan_iaas_abrak" "abrak-3" {
-  region = var.region
-  flavor = "g1-1-1-0"
-  name   = "${var.abrak_name}-3"
-  image {
-    type = "distributions"
-    name = "debian/12"
-  }
-  disk_size = 25
-}
-
-data "arvan_iaas_abrak" "get_abrak_id_1" {
+data "arvan_iaas_abrak" "get_abrak_id" {
   depends_on = [
-    arvan_iaas_abrak.abrak-1
+    arvan_iaas_abrak.abrak
   ]
   region = var.region
-  name   = "${var.abrak_name}-1"
+  count  = 3
+  name   = "${var.abrak_name}-${count.index}"
 }
 
-data "arvan_iaas_abrak" "get_abrak_id_2" {
-  depends_on = [
-    arvan_iaas_abrak.abrak-2
-  ]
-  region = var.region
-  name   = "${var.abrak_name}-2"
-}
-
-data "arvan_iaas_abrak" "get_abrak_id_3" {
-  depends_on = [
-    arvan_iaas_abrak.abrak-3
-  ]
-  region = var.region
-  name   = "${var.abrak_name}-3"
-}
-
-output "details-abrak-1" {
+output "details-abrak" {
   value = [
-    data.arvan_iaas_abrak.get_abrak_id_1,
-    data.arvan_iaas_abrak.get_abrak_id_2,
-    data.arvan_iaas_abrak.get_abrak_id_3
+    data.arvan_iaas_abrak.get_abrak_id
   ]
 }

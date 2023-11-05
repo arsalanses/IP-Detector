049859d502adf60a537064a358475e94026cf8c9
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-11-05 16:25:52 +0330

    Add subnet resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,9 @@ resource "arvan_iaas_abrak" "abrak" {
   key_name = "secure"
   image {
     type = "distributions"
-    name = "debian/12"
+    name = "ubuntu/22.04"
   }
-  disk_size = 25
+  disk_size  = 25
 }
 
 data "arvan_iaas_abrak" "get_abrak_id" {
@@ -22,7 +22,39 @@ data "arvan_iaas_abrak" "get_abrak_id" {
 }
 
 output "details-abrak" {
-  value = [
-    data.arvan_iaas_abrak.get_abrak_id
-  ]
+  value = data.arvan_iaas_abrak.get_abrak_id
 }
+
+resource "arvan_iaas_subnet" "subnet-1" {
+  region         = var.region
+  name           = "kube-subnet"
+  subnet_ip      = "192.168.0.0/28"
+  enable_gateway = true
+  gateway        = "192.168.0.1"
+  dns_servers = [
+    "1.1.1.1",
+    "9.9.9.9"
+  ]
+  enable_dhcp = true
+  dhcp {
+    from = "192.168.0.2"
+    to   = "192.168.0.14"
+  }
+}
+
+output "subnet-details" {
+  value = arvan_iaas_subnet.subnet-1
+}
+
+# resource "arvan_iaas_network_attach" "attach-network-abrak" {
+#   depends_on = [
+#     arvan_iaas_abrak.abrak,
+#     arvan_iaas_subnet.subnet-1
+#   ]
+
+#   count = 3
+
+#   region       = var.region
+#   abrak_uuid   = data.arvan_iaas_abrak.get_abrak_id[count.index].id
+#   network_uuid = arvan_iaas_subnet.subnet-1.network_uuid
+# }
